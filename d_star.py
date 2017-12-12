367852e11382c432a49770d0623b66546ceec750
Karahm Samuel Kim <karahmkim@gmail.com>
2017-12-11 20:24:40 -0500

    cost_plus_g function

--- d_star.py
+++ d_star.py
@@ -1,5 +1,7 @@
-
 #!/usr/bin/env python
+
+import numpy as np
+import pq
 
 def keyCompare(lhs,rhs):
     if lhs[0] < rhs[0]:
@@ -18,13 +20,19 @@ def calculateRhs(s):
 def calculateG(s):
     return 0
 
+def cost_plus_g(s1, s2):
+    return np.sqrt((s1.x - s2.x) ** 2 + (s1.y - s2.y) ** 2 + np.min((np.abs(s1.theta - s2.theta), 2 * np.pi - np.abs(s1.theta - s2.theta))) ** 2)
+
 def calculateKey(s):
     return 0 # TODO: Set this up
 
 def Initialize():
-    U = []
+    U = pq.Priority_queue
     k_m = 0
     for i in range(10): # TODO: Set this condition. for all s in S rhs(s) = g(s) = infinity
+    rhs(s_goal) = 0
+    U.push(s_goal, calculateKey(s_goal))
+    return U, k_m
         print 'A'
     rhs(s_goal) = 0
     U.insert(s_goal, calculateKey(s_goal))
