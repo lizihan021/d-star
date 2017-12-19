2b3a6f1691f296edcd13f63c33d652fc6c16764c
lizihan021 <lizihan@umich.edu>
2017-12-18 20:54:56 -0800

    actually not working

---
+++ dstar/astar/astar_forplot.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
+import openravepy
+
+#### YOUR IMPORTS GO HERE ####
+from Queue import PriorityQueue
+from pq import Node, Priority_Queue
+import math
+#### END OF YOUR IMPORTS ####
+
+if not __openravepy_build_doc__:
+    from openravepy import *
+    from numpy import *
+
+def waitrobot(robot):
+    """busy wait for robot completion"""
+    while not robot.GetController().IsDone():
+        time.sleep(0.01)
+
+def tuckarms(env,robot):
+    with env:
+        jointnames = ['l_shoulder_lift_joint','l_elbow_flex_joint','l_wrist_flex_joint','r_shoulder_lift_joint','r_elbow_flex_joint','r_wrist_flex_joint']
+        robot.SetActiveDOFs([robot.GetJoint(name).GetDOFIndex() for name in jointnames])
+        robot.SetActiveDOFValues([1.29023451,-2.32099996,-0.69800004,1.27843491,-2.32100002,-0.69799996]);
+        robot.GetController().SetDesired(robot.GetDOFValues());
+    waitrobot(robot)
+
+
+def ConvertPathToTrajectory(robot,path=[]):
+#Path should be of the form path = [q_1, q_2, q_3,...], where q_i = [x_i, y_i, theta_i]
+
+    if not path:
+	return None
+    # Initialize trajectory
+    traj = RaveCreateTrajectory(env,'')	
+    traj.Init(robot.GetActiveConfigurationSpecification())
+    for i in range(0,len(path)):
+	traj.Insert(i,numpy.array(path[i]))
+    # Move Robot Through Trajectory
+    planningutils.RetimeAffineTrajectory(traj,maxvelocities=ones(3),maxaccelerations=5*ones(3))
+    return traj
+
+
+if __name__ == "__main__":
+
+    env = Environment()
+    env.SetViewer('qtcoin')
+    collisionChecker = RaveCreateCollisionChecker(env,'ode')
+    env.SetCollisionChecker(collisionChecker)
+
+
+    env.Reset()
+    # load a scene from ProjectRoom environment XML file
+    env.Load('data/pr2test2.env.xml')
+    time.sleep(0.1)
+
+    # 1) get the 1st robot that is inside the loaded scene
+    # 2) assign it to the variable named 'robot'
+    robot = env.GetRobots()[0]
+
+    # tuck in the PR2's arms for driving
+    tuckarms(env,robot);
+
+
+    with env:
+        # the active DOF are translation in X and Y and rotation about the Z axis of the base of the robot.
+        robot.SetActiveDOFs([],DOFAffine.X|DOFAffine.Y|DOFAffine.RotationAxis,[0,0,1])
+
+        goalconfig = [2.6,-1.3,-pi/2]
+	start = time.clock()
+
+        #### YOUR CODE HERE ####
+        handles = []
+        def cal_cost(this_node, next_node, end_node):
+            d_theta1 = this_node.theta - next_node.theta
+            d_theta2 = end_node.theta - next_node.theta
+            g = sqrt((this_node.x-next_node.x)**2+(this_node.y-next_node.y)**2+(min(d_theta1, 2*pi-d_theta1))**2)
+            h = sqrt((end_node.x-next_node.x)**2+(end_node.y-next_node.y)**2+(min(d_theta2, 2*pi-d_theta2))**2)
+            cost_o = this_node.cost
+            next_node.cost = g + cost_o
+            return g + h + cost_o
+
+        def add_to_pq(q, this_p, end_p, grid, x_grid_num, y_grid_num, theta_num, points_plot):
+            this_node = grid[this_p[0]][this_p[1]][this_p[2]]
+            robot.SetActiveDOFValues([this_node.x, this_node.y, this_node.theta])
+            if not env.CheckCollision(robot):
+                
+                end_node = grid[end_p[0]][end_p[1]][end_p[2]]
+                points_plot.append( (1, [this_node.x, this_node.y,0]) )
+                # for i in [-1,0,1]:
+                #     for j in [-1,0,1]:
+                #         for k in [-1,0,1]:
+                #             if not (i==0 and j==0 and k==0):
+                #                 if this_p[0]+i >= 0 and this_p[0]+i < x_grid_num and\
+                #                     this_p[1]+j >= 0 and this_p[1]+j < y_grid_num and\
+                #                     this_p[2]+k >= 0 and this_p[2]+k < theta_num:
+                #                     next_node = grid[this_p[0]+i][this_p[1]+j][this_p[2]+k]
+                #                     if next_node.parentid == (-1,-1,-1):
+                #                         next_node.parentid = this_node.id
+                #                         q.put((cal_cost(this_node, next_node, end_node), [this_p[0]+i,this_p[1]+j,this_p[2]+k] ))
+                                        
+                if this_p[0]-1 >= 0:
+                    next_node = grid[this_p[0]-1][this_p[1]][this_p[2]]
+                    if next_node.parentid == (-1,-1,-1):
+                        next_node.parentid = this_node.id
+                        q.put((cal_cost(this_node, next_node, end_node), [this_p[0]-1,this_p[1],this_p[2]] ))
+                if this_p[0]+1 < x_grid_num:
+                    next_node = grid[this_p[0]+1][this_p[1]][this_p[2]]
+                    if next_node.parentid == (-1,-1,-1):
+                        next_node.parentid = this_node.id
+                        q.put((cal_cost(this_node, next_node, end_node), [this_p[0]+1,this_p[1],this_p[2]]))
+                if this_p[1]-1 >= 0:
+                    next_node = grid[this_p[0]][this_p[1]-1][this_p[2]]
+                    if next_node.parentid == (-1,-1,-1):
+                        next_node.parentid = this_node.id
+                        q.put((cal_cost(this_node, next_node, end_node), [this_p[0],this_p[1]-1,this_p[2]]))
+                if this_p[1]+1 < y_grid_num:
+                    next_node = grid[this_p[0]][this_p[1]+1][this_p[2]]
+                    if next_node.parentid == (-1,-1,-1):
+                        next_node.parentid = this_node.id
+                        q.put((cal_cost(this_node, next_node, end_node), [this_p[0],this_p[1]+1,this_p[2]]))
+                if this_p[2]-1 >= 0:
+                    next_node = grid[this_p[0]][this_p[1]][this_p[2]-1]
+                    if next_node.parentid == (-1,-1,-1):
+                        next_node.parentid = this_node.id
+                        q.put((cal_cost(this_node, next_node, end_node), [this_p[0],this_p[1],this_p[2]-1]))
+                if this_p[2]+1 < theta_num:
+                    next_node = grid[this_p[0]][this_p[1]][this_p[2]+1]
+                    if next_node.parentid == (-1,-1,-1):
+                        next_node.parentid = this_node.id
+                        q.put((cal_cost(this_node, next_node, end_node), [this_p[0],this_p[1],this_p[2]+1]))
+            else:
+                points_plot.append((0, [this_node.x, this_node.y,0]))
+        
+        table6 = env.GetBodyFromEnvironmentId(6)
+        table7 = env.GetBodyFromEnvironmentId(7)
+        new_pose6 = table6.GetTransform()
+        new_pose6[0][3] = 0
+        new_pose7 = table7.GetTransform()
+        new_pose7[0][3] = 2
+        new_pose7[1][3] = 0
+        table6.SetTransform(new_pose6)
+        table7.SetTransform(new_pose7)
+
+        o_pose = robot.GetTransform()
+        startconfig = [o_pose[0][3], o_pose[1][3], 0]
+        step_len = 0.1
+        x_grid_num = int(7.4/step_len)
+        y_grid_num = int(3.4/step_len)
+        theta_num = 4
+        grid = []
+        for i in range(x_grid_num):
+            line = []
+            for j in range(y_grid_num):
+                line.append([ Node(-3.7+i*step_len,-1.7+j*step_len, pi-2*pi/theta_num*k, (i,j,k), (-1,-1,-1),0) for k in range(theta_num)])
+            grid.append(line)
+        start_p = [int(round((startconfig[0]+3.7)/step_len)), int(round((startconfig[1]+1.7)/step_len)), theta_num/2]
+        end_p = [int(round((goalconfig[0]+3.7)/step_len)), int(round((goalconfig[1]+1.7)/step_len)), 3*theta_num/4]
+
+        #### Implement your algorithm to compute a path for the robot's base starting from the current configuration of the robot and ending at goalconfig. The robot's base DOF have already been set as active. It may be easier to implement this as a function in a separate file and call it here.
+        q = Priority_Queue()
+        grid[start_p[0]][start_p[1]][start_p[2]].parentid = (-2,-2,-2)
+        points_plot = []
+        add_to_pq(q, start_p, end_p, grid, x_grid_num, y_grid_num, theta_num, points_plot)
+        next_p = q.get()[1]
+        while next_p != end_p:
+            add_to_pq(q, next_p, end_p, grid, x_grid_num, y_grid_num, theta_num, points_plot)
+            next_p = q.get()
+            f_cost = next_p[0]
+            next_p = next_p[1]
+            # print(next_p)
+        print("final cost", f_cost)
+        #### Draw the X and Y components of the configurations explored by your algorithm
+
+ 	path = [] #put your final path in this variable
+        pp = grid[end_p[0]][end_p[1]][end_p[2]]
+        while pp.parentid != (-2,-2,-2):
+            path.append([pp.x,pp.y,pp.theta])
+            id_p = pp.parentid
+            # print(id_p)
+            pp = grid[id_p[0]][id_p[1]][id_p[2]]
+
+        robot.SetActiveDOFValues(startconfig)
+        path.append(startconfig)
+        path = path[::-1]
+        pts = ()
+        for p in path:
+            pts = pts+((p[0], p[1], 0.05),)
+        pts = array(pts)
+        
+        handles.append(env.drawlinestrip(points=pts,linewidth=3.0,
+                                         colors=array(((0,0,0)))))
+        for ppl in points_plot:
+            if ppl[0] == 1:
+                handles.append(env.plot3(points=array( ppl[1]),pointsize=5.0,
+                            colors=array((0,0,1))))
+            else:
+                if not (1, ppl[1]) in points_plot:
+                    handles.append(env.plot3(points=array( ppl[1]),pointsize=5.0,
+                                colors=array((1,0,0))))
+
+
+
+        #### END OF YOUR CODE ###
+	end = time.clock()
+	print "Time: ", end - start
+
+        # Now that you have computed a path, convert it to an openrave trajectory 
+	traj = ConvertPathToTrajectory(robot, path)
+
+	# Execute the trajectory on the robot.
+	if traj != None:
+	    robot.GetController().SetPath(traj)
+
+
+    waitrobot(robot)
+
+    raw_input("Press enter to exit...")
+
---
+++ dstar/grid.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+#defines a basic node class
+import numpy as np
+
+class Node:
+    def __init__(self,x_in,y_in,theta_in, id_in, g_in, rhs_in, coll_in):
+        self.x = x_in # config position
+        self.y = y_in
+        self.theta = theta_in
+        self.id = id_in
+        self.g = g_in
+        self.rhs = rhs_in
+        self.coll = coll_in
+
+    def succ(self, grid):
+        successors = []
+        for i in range(-1,2):
+            for j in range(-1,2):
+                for k in range(-1,2):
+                    if np.linalg.norm([i,j,k]) == 1:
+                        if  self.id[0]+i >= 0 and self.id[0]+i < grid.x_grid_num and \
+                            self.id[1]+j >= 0 and self.id[1]+j < grid.y_grid_num:
+                            successors.append( grid.grid[self.id[0]+i][self.id[1]+j][(self.id[2]+k) % 4])
+        return successors
+
+    def pred(self, grid):
+        return self.succ(grid)
+
+    def j_id(self):
+        return str(self.id)[1:-1]
+
+    def __str__(self):
+        return "\tNode id"+ str(self.id)+":"+ "x ="+ str(self.x)+ "y ="+str(self.y)+ "theta ="+ str(self.theta)+ "parentid:"+ str(self.parentid)
+
+class Grid:
+    def __init__(self, startconfig, goalconfig):
+        self.step_len = 0.1
+        self.x_len = 3.7
+        self.y_len = 1.7
+        # 0,      1,        2,  3,
+        # -np.pi, -np.pi/2, 0, np.pi/2
+        self.theta_num = 4
+        self.x_grid_num = int(2*self.x_len/self.step_len)
+        self.y_grid_num = int(2*self.y_len/self.step_len)
+        self.grid = []
+        # initialize the grid
+        for i in range(self.x_grid_num):
+            line = []
+            for j in range(self.y_grid_num):
+                line.append([ Node(-self.x_len+i*self.step_len,-self.y_len+j*self.step_len, np.pi/2*k - np.pi, (i,j,k),  np.inf, np.inf, False) for k in range(self.theta_num)])
+            self.grid.append(line)
+        # find start point and end point in the grid.
+        self.start_n = self.getNode(self.config_point(startconfig))
+        self.goal_n = self.getNode(self.config_point(goalconfig))
+        self.start_n_no_change = Node(self.start_n.x,self.start_n.y, self.start_n.theta, self.start_n.id,  np.inf, np.inf, False)
+
+
+    def getNode(self, point):
+        return self.grid[point[0]][point[1]][point[2]]
+
+    def angle_index(self, angle):
+        return int((angle + np.pi)/(np.pi/2) % 4)
+
+    def index_angle(self, idx):
+    	return idx*np.pi/2 - np.pi
+
+    def config_point(self, config):
+        return [int(round((config[0]+self.x_len)/self.step_len)), int(round((config[1]+self.y_len)/self.step_len)), self.angle_index(config[2])]
+
+    def point_config(self, point):
+    	return [point[0]*self.step_len - self.x_len, point[1]*self.step_len - self.y_len, self.index_angle(point[2])]
